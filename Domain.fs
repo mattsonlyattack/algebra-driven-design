95b85073ccc5f5403030dcf9c7a831a240a62424
Matt Davis <mattdavis@quickenloans.com>
2021-05-25 17:23:53 -0400

    breaking out some ctors to simplify poi

--- Domain.fs
+++ Domain.fs
@@ -2,6 +2,8 @@ module Domain
 
 type Undefinied = exn
 
+// types
+
 type Challenge = Undefinied
 type Clue = Undefinied
 type Distance = Undefinied
@@ -10,10 +12,18 @@ type Photo = Undefinied
 type Point = Undefinied
 type Reward = Undefinied
 
+// constructors
+
 type getRewards = Challenge -> Input -> Reward
 
-type pointOfInterest = Clue -> Point -> Distance -> Reward -> Challenge
+type pointOfInterest = Clue -> Point -> Distance -> Reward
 
 type photo = Point -> Photo -> Input
 
 type within = Point -> Point -> Distance -> bool
+
+type clue = Clue -> Challenge -> Challenge
+
+type reward = Reward -> Challenge
+
+type photoWithin = Point -> Distance -> Challenge -> Challenge
