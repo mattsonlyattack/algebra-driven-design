0eefa928a294dc8df463d585cfc743ea5468939e
Matt Davis <matt@mattsonlyattack.net>
2021-05-25 21:58:44 -0400

    I think I'm mostly keeping up...

--- Domain.fs
+++ Domain.fs
@@ -18,7 +18,10 @@ type Reward = Undefinied
 
 type getRewards = Challenge -> Input -> Reward
 
-type pointOfInterest = Clue -> Point -> Distance -> Reward
+// semantics from pg104
+// ∀ (c :: Clue) (p :: Point) (d :: Distance) (r :: ↪ Reward).
+// pointOfInterest c p d r = clue c (gate (photoWithin p d) (reward r))
+type pointOfInterest = Clue -> Point -> Distance -> Reward -> Challenge
 
 type photo = Point -> Photo -> Input
 
@@ -26,6 +29,8 @@ type within = Point -> Point -> Distance -> bool
 
 type clue = Clue -> Challenge -> Challenge
 
+// experience suggests that this should be a terminal constructor — 
+// one which builds a Challenge ex nihilo
 type reward = Reward -> Challenge
 
 type photoWithin = Point -> Distance -> InputFilter
@@ -35,3 +40,10 @@ type photoAbove = Altitude -> InputFilter
 type matches = InputFilter -> Input -> bool
 
 type gate = InputFilter -> Challenge -> Challenge
+
+// we should expect all of the usual boolean values and operations to lift over input filters
+type always = InputFilter
+type never = InputFilter
+type andF = InputFilter -> InputFilter -> InputFilter
+type orF = InputFilter -> InputFilter -> InputFilter
+type notF = InputFilter -> InputFilter
